ca0ac7173717ce090efe695203849d9a8239727b
Daniel Slattery <daniel.slattery1@gmail.com>
2021-11-21 22:55:50 +0000

    added movie fetch

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import * as ApiClient from "./Services/ApiClient";
 
 const App = () => {
-  return <h1>Movie App</h1>;
+  const [movies, setMovies] = useState([
+    { backdrop_path: "cinER0ESG0eJ49kXlExM0MEWGxW.jpg" },
+  ]);
+
+  useEffect(() => {
+    ApiClient.getDiscoverMovies().then((newMovies) => {
+      console.log(newMovies);
+      setMovies(newMovies);
+    });
+
+    console.log("here", movies);
+  }, []);
+
+  return (
+    <div>
+      <h1>Movies List</h1>
+      {movies &&
+        movies.map((movie) => (
+          <img
+            key={movie.id}
+            src={"https://image.tmdb.org/t/p/w300/" + movie.backdrop_path}
+          />
+        ))}
+    </div>
+  );
 };
 
 export default App;
