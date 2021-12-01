53a121c4a893e93fc28484c225247130a2ef96a2
Daniel Slattery <daniel.slattery1@gmail.com>
2021-12-01 21:50:42 +0000

    added updateState function

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
 import React, { useState, useEffect } from "react";
-import { getDiscoverMovies } from "./Services/ApiClient";
-import "./app.css";
+import {
+  getDiscoverMovies,
+  getCategories,
+  getMoviesFromCategory,
+} from './Services/ApiClient';
+import './app.css';
 import MovieList from './components/MovieList/MovieList';
 
 const App = () => {
-  const [movies, setMovies] = useState([]);
+  const [status, setStatus] = useState(true);
+  const [movies, setMovies] = useState({});
+  const [lists, setLists] = useState({ myList: [] });
+
+
+  const updateState = (name, list) => {
+    setMovies(movies =>
+      list.reduce(
+        (acc, mov) => ({
+          ...acc,
+          [mov.id]: Object.assign(mov, { mylist: false }),
+        }),
+        movies
+      )
+    );
+    setLists(lists => ({ ...lists, [name]: list.map(mov => mov.id) }));
+  };
+
 
   useEffect(() => {
-    const fetchMovies = async () => {
+    const fetchDiscoverMovies = async () => {
       const newMovies = await getDiscoverMovies();
-      setMovies(newMovies);
+      updateState('discover', newMovies);
     };
-    fetchMovies();
-    console.log('here', movies);
+
+    const fetchCategoryMovieLists = async () => {
+      const categories = await getCategories();
+      const daniel = await categories.map(({ id, name }) => getMoviesFromCategory(id))
+      console.log('here', daniel)
+     const dano = await daniel.map(({id, name}) => updateState(name, newMovies))
+
+    }
+
+    fetchDiscoverMovies();
+    fetchCategoryMovieLists();
+    // fetchCategories();
   }, []);
 
   return (
     <div className='App'>
       <h1>Discover Now</h1>
       <MovieList movies={movies} />
+      {console.log(lists)}
+      <MovieList movies={lists.Action} />
+      <MovieList movies={movies} />
+
     </div>
   );
 };
--- src/components/MovieList/MovieList.js
+++ src/components/MovieList/MovieList.js
@@ -4,8 +4,8 @@ import MovieTile from '../MovieTile/MovieTile';
 const MovieList = ({ movies }) => {
   return (
     <div className='MovieList'>
-      {movies &&
-        movies.map((movie) => <MovieTile key={movie.id} movie={movie} />)}
+      {/* {movies &&
+        movies.map((movie) => <MovieTile key={movie.id} movie={movie} />)} */}
     </div>
   );
 };
