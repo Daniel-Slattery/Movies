654ea899effe9dbdd19e698f5b0e9143cc706aec
Daniel Slattery <daniel.slattery1@gmail.com>
2021-12-05 21:04:50 +0000

    added movie categories titles and updated styling

--- src/components/MovieList/MovieList.js
+++ src/components/MovieList/MovieList.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import MovieTile from '../MovieTile/MovieTile';
+import './style.css';
 
 const MovieList = ({ movies, title }) => (
   <div>
     {movies.length > 0 && (
       <div>
         <div className="list_title">
-          {title === 'mylist' ? 'My List' : title.charAt(0).toUpperCase() + title.slice(1)}
+         <h2>{title === 'mylist' ? 'My List' : title.charAt(0).toUpperCase() + title.slice(1)}</h2>
         </div>
         <div className="list_scroll">
           {movies.map(movie => (
