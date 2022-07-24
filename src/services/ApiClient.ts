97f1108cdfca5b2d6083159a54fe18f04c92fbf5
Daniel Slattery <daniel.slattery1@gmail.com>
2022-07-24 17:06:58 +0100

    converted ApiClient from .js to .ts

--- src/services/ApiClient.ts
+++ src/services/ApiClient.ts
@@ -2,9 +2,9 @@ const BASE_URL = "https://movied.herokuapp.com";
 
 export const getDiscoverMovies = () => fetchRequest(`discover`);
 export const getCategories = () => fetchRequest(`categories`);
-export const getMoviesFromCategory = (id) => fetchRequest(`categories/${id}`);
+export const getMoviesFromCategory = (id: string) => fetchRequest(`categories/${id}`);
 
-const fetchRequest = async (url) => {
+const fetchRequest = async (url: string) => {
   const response = await fetch(`${BASE_URL}/${url}`);
   return await response.json();
 };
